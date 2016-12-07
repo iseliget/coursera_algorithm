c4b515bbbfd771cc150a4d0b25968c4a2d3af937
Chao <chaoguo94@gmail.com>
2016-12-06 22:02:01 -0500

    Update quicksort.py

--- quicksort.py
+++ quicksort.py
@@ -23,3 +23,66 @@ def pivoting(array):
 	result_array[left_pointer] = pivot
 
 	return result_array
+
+def partition(array, leftBoundary, rightBoundary):
+	'''
+	partition a subarray in O(n) time
+	essentially the same as pivoting
+	but does not need extra space
+	'''
+	pivot = array[leftBoundary]
+	i = leftBoundary + 1
+
+	for j in range(i, rightBoundary+1):
+		if array[j] < pivot:
+			array[j], array[i] = array[i], array[j]
+			i += 1
+
+	array[leftBoundary], array[i-1] = array[i-1], array[leftBoundary]
+	return i - 1 # position of pivot in new array
+
+def quickSort(array,start,end):
+	if len(array) == 1:
+	 	return array
+	if start < end:
+		pivot_index = partition(array,start,end)
+		quickSort(array,start,pivot_index-1)
+		quickSort(array,pivot_index+1,end)
+		return array
+
+
+# print(partition([1],0,0))
+
+# print(partition([1,1,1,1],0,3))
+# print(partition([2,2,1,1],0,3))
+
+# print(partition([1,2],0,1))
+# print(partition([2,1],0,1))
+
+# print(partition([9,8,5],1,2))
+# print(partition([9,8,5],0,2))
+
+# print(partition([3,2,7,4,5],0,4))
+# print(partition([3,2,7,4,5],1,3))
+# print(partition([1,1,1,1,1],0,4))
+# print(partition([3,2,7,4,5],3,4))
+# print(partition([1,1,1,1,2],0,4))
+
+# print(quickSort([1,1,1,1,2],0,4))
+# print(quickSort([1,1,1,1,2],1,4))
+
+# print(quickSort([1,1],0,1))
+# print(quickSort([1,1,1],0,2))
+# print(quickSort([1,1,1,1],0,3))
+
+# print(quickSort([1,2],0,1))
+# print(quickSort([1,1,2],0,2))
+# print(quickSort([1,1,1,2],0,3))
+
+# print(quickSort([1,1,1,1,2],0,4))
+# print(quickSort([1,1,1,1,2],0,-1))
+# print(quickSort([1,1,1,1,2],1,4))
+# print(quickSort([1,1,1,1,1,2],0,5))
+
+# print(quickSort([5,3,4,6,2,1,7],0,6))
+# print(quickSort([13,45,23,75,36,87],0,5))
