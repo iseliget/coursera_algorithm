4c4e6c52aeac86511c5a72642ab7e34adf36fc18
Chao <chaoguo94@gmail.com>
2016-12-13 19:22:35 -0500

    Update mincut.py

--- mincut.py
+++ mincut.py
@@ -1,10 +1,14 @@
 # suppose the adjacency list is a list of list
 # e.g. [[],[2,4],[1,3,4],[2,4],[1,2,3]]
 import random
+import csv
 
 def contraction(adj_list):
-	u = random.randint(1,len(adj_list)-1)
-	v = adj_list[u][random.randint(0,len(adj_list[u]))-1]
+	u = 0
+	while len(adj_list[u]) == 0:
+		u = random.randint(1,len(adj_list)-1)
+
+	v = adj_list[u][random.randint(0,len(adj_list[u])-1)]
 
 	vertex_1 = min(u,v)
 	vertex_2 = max(u,v)
@@ -22,5 +26,25 @@ def contraction(adj_list):
 
 	return adj_list
 
-print(contraction([[],[2,3,3],[1,3],[2,1,1]]))
-print(contraction([[],[2,4],[1,3,4],[2,4],[1,2,3]]))
+def mincut(adj_list):
+	while adj_list.count([]) < len(adj_list) - 2:
+		#print(adj_list)
+		adj_list = contraction(adj_list)
+	#print(adj_list)
+	vertices_left = [x for x in adj_list if x!= []]
+	print(len(vertices_left[0]))
+	return len(vertices_left[0])
+
+# mincut([[],[2,3,4,7],[1,3,4],[1,2,4],[1,2,3,5],[4,6,7,8],[5,7,8],[1,5,6,8],[5,6,7]])
+# mincut([[],[4,2,7,3],[4,1,3],[1,2,4],[5,1,2,3],[8,7,6,4],[8,5,7],[6,8,5,1],[7,6,5]])
+# mincut([[],[2,3,4],[1,3,4],[1,2,4],[1,2,3,5],[4,6,7,8],[5,7,8],[5,6,8],[5,6,7]])
+# mincut([[],[3,4,2],[1,4,3],[1,2,4],[5,3,2,1],[4,8,6,7],[8,7,5],[5,8,6],[5,7,6]])
+
+graph = [[]]
+with open('graph2.txt','rb') as f:
+	reader = csv.reader(f,delimiter='\t')
+	for row in reader:
+		# print(row)
+		graph.append([int(x) for x in row[1:-1]])
+# print(graph[0:3])
+mincut(graph)
