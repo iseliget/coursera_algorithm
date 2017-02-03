ba2d40ddb972ceadc2e6ed319ee5d1b7295a1bb4
Chao <chaoguo94@gmail.com>
2017-02-03 22:17:38 +0800

    Update kosaraju.py

--- course_2/week_1/kosaraju.py
+++ course_2/week_1/kosaraju.py
@@ -1,19 +1,46 @@
 import copy
 
+t = 0 # number of nodes processed by DFS so far
+s = 0 # most recent source from which a DFS was initiated
+	
+graph = [[3], [7], [5], [6], [1], [8], [0], [4, 5], [2, 6]] # The graph used as an example during the lecture
+
+finishing_time = [0]*len(graph)
+leaders = [0]*len(graph)
+explored_nodes = []
+
+def dfs(graph,node):
+    global t
+    global s
+    global explored_nodes
+    global leaders
+
+    explored_nodes.append(node)
+    leaders[node] = s
+
+    for adj_node in graph[node]:
+        if adj_node not in explored_nodes:
+            dfs(graph,adj_node)
+
+    t += 1
+    finishing_time[node] = t
+    
+
 def reverse(graph):
-    reversed_graph = copy.deepcopy(graph) # cannot use graph[:] because the List "graph" contains Lists
-    reversed_nodes = []
+	reversed_graph = []
+	for i in range(0,len(graph)):
+		reversed_graph.append([])
 
-    for node in range(0,len(reversed_graph)):
+	for i in range(0,len(graph)):
+		for adj_node in graph[i]:
+			reversed_graph[adj_node].append(i)
 
-        for adj_node in reversed_graph[node][:]: # [:] is important
+	return reversed_graph
 
-            if adj_node not in reversed_nodes:
-                reversed_graph[adj_node].append(node)
-                reversed_graph[node].remove(adj_node)
-            else:
-                pass
+# First pass
+reversed_graph = reverse(graph)
+for i in range(len(reversed_graph)-1,-1,-1):
+	if i not in explored_nodes:
+		dfs(reversed_graph,i)
+print(finishing_time)
 
-        reversed_nodes.append(node)
-
-    return reversed_graph
